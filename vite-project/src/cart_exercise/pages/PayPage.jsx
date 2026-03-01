bcfb6095dcd53196b7bfb6b9a9d866aee24fe65b
cahves1 <athoschaveslopesdealmeida@gmail.com>
2026-03-01 11:01:55 -0300

    01/03/2026 - Cart exercise finished

--- vite-project/src/cart_exercise/pages/PayPage.jsx
+++ vite-project/src/cart_exercise/pages/PayPage.jsx
@@ -1,3 +1,4 @@
+import '../cart.css'
 import { useContext } from "react"
 import { CartContext }  from '../context/CartContext'
 
@@ -7,14 +8,14 @@ const PayPage = () => {
   return (
     <main className='pay_page'>
       <header>
-        <h1>Cart</h1>
         <div className="btns-container">
           <button onClick={() => setCurrentPage('shop')}>Shop</button>
           <button onClick={() => setCurrentPage('cart')}>Cart</button>
         </div>
+        <h1>Cart</h1>
       </header>
       <div className="final_container">
-        <h3>Final Price: ${calculateTotalPrice()}</h3>
+        <h2>Final Price: ${calculateTotalPrice().toFixed(2)}</h2>
         <button>Buy</button>
       </div>
     </main>
